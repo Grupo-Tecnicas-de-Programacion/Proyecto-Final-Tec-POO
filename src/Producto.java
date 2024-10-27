4c40bed12b6f009bb522352177e9dbc42b8f036a
Jhon BC <jhonabc.p@gmail.com>
2024-10-26 22:58:41 -0500

    Llenado de la clase Producto

--- src/Producto.java
+++ src/Producto.java
@@ -1,12 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.io.Serializable;
 
-/**
- *
- * @author EQUIPO
- */
-public class Producto {
+public class Producto implements Serializable{
+    private String nombre;
+    private double precio;
+    private String categoria;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, double precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void actualizarPrecio(double nuevoPrecio){
+        if (nuevoPrecio > 0) {
+            this.precio = nuevoPrecio;
+        }else{
+            System.out.println("El precio debe ser mayor que 0.");
+        }
+        
+    }
     
+    @Override
+    public String toString() {
+        return "Producto:  " + 
+               "Nombre = " + this.getNombre() + " | " + 
+               "Precio = " + this.getPrecio() + " | " +
+               "Categoria = " + this.getCategoria() + ".";
+    }
 }
