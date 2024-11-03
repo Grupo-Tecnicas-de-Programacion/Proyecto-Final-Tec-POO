46370e3d78749a61bf5ecd5e708b2052b207de98
Jhon BC <jhonabc.p@gmail.com>
2024-11-02 23:57:33 -0500

    Se agrego la clase del administrador que podrá ser el que pueda ver los reportes

---
+++ src/Administrador.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+
+
+public class Administrador extends Usuario{
+    private ArrayList<Reporte> reportes;
+
+    public Administrador() {
+        this.reportes = new ArrayList<>();
+    }
+
+    public Administrador(ArrayList<Reporte> reportes, String nombreUsuario, String constrasenia) {
+        super(nombreUsuario, constrasenia);
+        this.reportes = new ArrayList<>();
+    }
+
+    public ArrayList<Reporte> getReportes() {
+        return reportes;
+    }
+
+    public void setReportes(ArrayList<Reporte> reportes) {
+        this.reportes = reportes;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrador{" + "reportes=" + reportes + '}';
+    }
+    
+    
+    public ArrayList<Reporte> obtenerTodosLosReportes(){
+        return this.getReportes();
+    }
+    
+}
